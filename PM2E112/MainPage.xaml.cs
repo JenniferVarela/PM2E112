a1b7e079dfd846a5bf9c04c38c9f9b9f83f16298
JenniferVarela <jnnfrvarela@gmail.com>
2022-06-18 14:56:19 -0600

    Ya toma Fotografia y la muestra en el image.

--- PM2E112/MainPage.xaml.cs
+++ PM2E112/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Plugin.Media;
 
 namespace PM2E112
 {
@@ -30,9 +31,25 @@ namespace PM2E112
 
         }
 
-        private void btnFoto_Clicked(object sender, EventArgs e)
+        private async void btnFoto_Clicked(object sender, EventArgs e)
         {
+            var fotografia = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "MisFotos",
+                Name = "test.jpg",
+                SaveToAlbum = true,
+            });
 
+
+            await DisplayAlert("path directorio", fotografia.Path, "ok");
+
+            if (fotografia != null)
+            {
+                fotoSitio.Source = ImageSource.FromStream(() =>
+                {
+                    return fotografia.GetStream();
+                });
+            }
         }
     }
 }
