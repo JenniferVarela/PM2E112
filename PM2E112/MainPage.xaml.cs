fbdc62e901e77da008b0b055b622a78945be5b52
JenniferVarela <jnnfrvarela@gmail.com>
2022-06-18 14:35:37 -0600

    Agregar archivos de proyecto.

---
+++ PM2E112/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PM2E112
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnList_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalir_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFoto_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
